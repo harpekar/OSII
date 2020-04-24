c1c39e01fd70f9087abb0b893d2dc22acbb8c0a6
Karen <karenl.harper88@gmail.com>
2020-04-23 23:54:13 -0700

    Determined how to tell architecture size

---
+++ Conc1/concurrency1.c
@@ -0,0 +1,22 @@
+//A small assignment to practice using pthread to access a single event buffer using multiple threads
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <pthread.h>
+#include "mt19937ar.h"
+
+
+int main() {
+
+
+    if (sizeof(int*) == 4){
+        
+    }
+    else {
+
+    }
+    
+    return 0;
+}
