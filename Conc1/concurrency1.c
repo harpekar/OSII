c42fa1bab18360289c52c7084b632e1fb9b57d9b
Karen <karenl.harper88@gmail.com>
2020-10-16 17:06:35 -0700

    Random number generation working in appropriate intervals

--- Conc1/concurrency1.c
+++ Conc1/concurrency1.c
@@ -3,22 +3,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 #include <pthread.h>
 #include "mt19937ar.h"
 
+struct Item {
+    int value;
+    int workTime;
+};
+
+float randomValue() { //Computes a random value between 0 and 1, using the appropriate method
+
+    unsigned a;
+    float result;
+
+    if (sizeof(int*) == 4){ //If the system is 32bit
+        result = genrand_real3();         
+    }
+    else {
+        printf("rdrand \n");
+        asm ("RDRAND %%ax" : "=r" (a) );
+        result = a/(pow(2,16)); 
+    }
+
+    return result;
+}
 
 int main() {
 
-    if (sizeof(int*) == 4){ //If the system is 32bit
-     
-    }
+    if (sizeof(int*) == 4) {init_genrand(time(NULL));} //Initialize the Mersenne Twister if it's needed
     
-    else {
-        unsigned a;
-        asm ("RDRAND %%eax" : "=r" (a) );
+    float a;
     
-    }
+    a = randomValue();
+
+    printf("%f \n", a);
     
     return 0;
 }
